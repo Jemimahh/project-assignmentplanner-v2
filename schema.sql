e1745643f2e29d0be6d25ab5856cf11afc8806b2
ntun <ntun@iwu.edu>
2018-11-12 00:50:40 -0600

    Added test cases for add, delete, edit features

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ drop table if exists assignments;
 create table assignments (
   id integer primary key autoincrement,
   title text not null,
-  class text not null,
+  course text not null,
   category text not null,
   duedate text not null,
   description text not null
